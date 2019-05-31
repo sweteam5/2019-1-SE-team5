fc74d976a25c960c438facec294d400bd388d663
SangHyune546 <48785671+SangHyune546@users.noreply.github.com>
2019-05-31 14:52:55 +0900

    Update GUI_YUTPAN.java
    맨왼쪽 보드 위치 수정2

--- src/GUI_YUTPAN.java
+++ src/GUI_YUTPAN.java
@@ -218,10 +218,10 @@ public class GUI_YUTPAN implements ActionListener {
 
         // 맨 왼쪽 보드
         btn_Board[10][0].setBounds(35, 476, 50, 50);
-        btn_Board[2][0].setBounds(36, 377, 50, 50);
-        btn_Board[4][0].setBounds(36, 297, 50, 50);
-        btn_Board[6][0].setBounds(36, 218, 50, 50);
-        btn_Board[8][0].setBounds(36, 138, 50, 50);
+        btn_Board[8][0].setBounds(36, 377, 50, 50);
+        btn_Board[6][0].setBounds(36, 297, 50, 50);
+        btn_Board[4][0].setBounds(36, 218, 50, 50);
+        btn_Board[2][0].setBounds(36, 138, 50, 50);
         btn_Board[0][0].setBounds(35, 39, 50, 50);
 
         // 맨 위쪽 보드
